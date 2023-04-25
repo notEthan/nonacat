fdcadfc7aecce348bc7d8c07be3c6d551938a02d
Ethan <ethan@unth.net>
2023-04-25 00:02:23 -0700

    de-cruft bundle-generated gem

--- Gemfile
+++ Gemfile
@@ -1,8 +1,3 @@
-# frozen_string_literal: true
-
 source "https://rubygems.org"
 
-# Specify your gem's dependencies in nonacat.gemspec
 gemspec
-
-gem "rake", "~> 13.0"
--- lib/nonacat.rb
+++ lib/nonacat.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
-require_relative "nonacat/version"
+require("nonacat/version")
 
 module Nonacat
-  class Error < StandardError; end
-  # Your code goes here...
 end
